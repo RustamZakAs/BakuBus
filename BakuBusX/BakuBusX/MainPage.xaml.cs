380fcf57282143e8d17d5de7662f6d3de197d385
RustamZakAs <rostik055@gmail.com>
2019-02-18 02:24:24 +0400

    Add project files.

---
+++ BakuBusX/BakuBusX/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BakuBusX.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BakuBusX
+{
+    public partial class MainPage : ContentPage
+    {
+        string appName = "BakuBusX";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Go_Map_Clicked(object sender, EventArgs e)
+        {
+            BUSAPI bus = new BUSAPI();
+            bus.GetBusAPI();
+
+            if (bus.BUS != null)
+            {
+                string lat = bus.BUS.Attributes[0].LATITUDE.ToString();
+                string lon = bus.BUS.Attributes[0].LONGITUDE.ToString();
+                await DisplayAlert(appName, lat + " " + lon, "Cancel");
+            }
+            else
+            {
+                await DisplayAlert(appName, "Error", "Cancel");
+            }
+
+            
+        }
+
+        private void Close_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ BakuBusX/BakuBusX/Services/BUSAPI.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Text;
+
+namespace BakuBusX.Services
+{
+    class BUSAPI
+    {
+        [JsonProperty("BUS")]
+        public BusAPI BUS { get; set; }
+
+        public BUSAPI GetBusAPI()
+        {
+            BUSAPI buss = new BUSAPI();
+            string info = string.Empty;
+
+            try
+            {
+                info = new WebClient().DownloadString("https://www.bakubus.az/az/ajax/apiNew1");
+                buss = JsonConvert.DeserializeObject<BUSAPI>(info);
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+            BUS = buss.BUS;
+            return buss;
+        }
+    }
+
+    class BusAPI
+    {
+        [JsonProperty("@attributes")]
+        public ObservableCollection<Attributes> Attributes { get; set; }
+    }
+
+    class Attributes
+    {
+        public int BUS_ID { get; set; }
+        public string PLATE { get; set; }
+        public string DRIVER_NAME { get; set; }
+        public string CURRENT_STOP { get; set; }
+        public string PREV_STOP { get; set; }
+        public int SPEED { get; set; }
+        public string BUS_MODEL { get; set; }
+        public double LATITUDE { get; set; }
+        public double LONGITUDE { get; set; }
+        public string ROUTE_NAME { get; set; }
+        public uint LAST_UPDATE_TIME { get; set; }
+        public int DISPLAY_ROUTE_CODE { get; set; }
+        public int SVCOUNT { get; set; }
+    }
+}
